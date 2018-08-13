fc52ae9391f7cef8f1b6dc3a9cc05b7834a08779
Jose Virgen <heavytech@Joses-MacBook-Pro.local>
2018-08-12 18:15:58 -0700

    sum w/different loops

--- Sum.java
+++ Sum.java
@@ -5,7 +5,7 @@ public class Sum {
 	/*
 	 * 1) Write three functions that compute the sum of the numbers in a given list using a for-loop,
 	 *  a while-loop, and recursion.
-	 *  @Author HeavyTech 2018
+	 *  @Author HeavyTech 2018 
 	 */
 
 	public static void main(String[] args) {
