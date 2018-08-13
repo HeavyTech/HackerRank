dbb7e1883c890682441d2d3012b93d672b386a78
Jose Virgen <heavytech@Joses-MacBook-Pro.local>
2018-08-12 18:14:31 -0700

    compute fibonacci series

---
+++ Fibannacci.java
@@ -0,0 +1,35 @@
+package HackerRank;
+
+import java.util.Scanner;
+/*
+ * 3) Write a function that computes the list of the first 100 Fibonacci numbers.
+ *  By definition, the first two numbers in the Fibonacci sequence are 0 and 1, and each
+ * subsequent number is the sum of the previous two. As an example, here are the first 10 Fibonnaci 
+ * numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, and 34.
+ * 
+ * 
+ */
+public class Fibannacci {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter A Fibonacci");
+		
+		int num = input.nextInt();
+		System.out.println(fibonacciSeries(num));
+		
+	
+	}
+	
+	public static int fibonacciSeries(int n) {
+		if(n < 0) {
+			return n;
+		}
+		if(n < 1) {
+			return 1;
+		}
+		return fibonacciSeries(n-1) + fibonacciSeries(n-2);
+		
+	}
+
+}
